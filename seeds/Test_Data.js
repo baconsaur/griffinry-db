f00721e47df65d52aa5e4729c231fce783c2f464
Jonathan Casey <jon_casey@me.com>
2016-01-09 14:09:58 -0700

    update promises in seed file

--- seeds/Test_Data.js
+++ seeds/Test_Data.js
@@ -1,51 +1,83 @@
 
 exports.seed = function(knex, Promise) {
-  return Promise.join(
-
-    // Users Table
-    knex('Users').del(),
+  return knex('User_Resources').del()
+  .then(function(){
+  return knex('Adoptions').del();
+}).then(function(){
+  return knex('Resources').del();
+}).then(function(){
+  return knex('Resource_Rarity').del();
+}).then(function(){
+  return knex('Pets').del();
+}).then(function(){
+  return knex('Pet_Colors').del();
+}).then(function(){
+  return knex('Users').del();
+}).then(function(){
+    return Promise.all([
     knex('Users').insert({
+      id: 1,
       User_Name: 'JDrill',
       Email: 'JJDrill@gmail.com',
       DOB: '1995/01/01'
     }),
     knex('Users').insert({
+      id: 2,
       User_Name: 'AshK',
       Email: 'tetrapteryxgames@gmail.com',
       DOB: '1995/01/01'
     }),
     knex('Users').insert({
+      id: 3,
       User_Name: 'DavidS',
       Email: 'DavidS@gmail.com',
       DOB: '1995/01/01'
     }),
     knex('Users').insert({
+      id: 4,
       User_Name: 'JonC',
       Email: 'JonC@gmail.com',
       DOB: '1995/01/01'
     }),
-
-    // Pets Table
-    knex('Pets').del(),
+  ]);
+}).then(function(){
+  return Promise.all([
+    knex('Pet_Colors').insert({Color: 'Red'}),
+    knex('Pet_Colors').insert({Color: 'Gold'}),
+    knex('Pet_Colors').insert({Color: 'Blue'}),
+    knex('Pet_Colors').insert({Color: 'Black'}),
+    knex('Pet_Colors').insert({Color: 'Yellow'})
+  ]);
+}).then(function(){
+  return Promise.all([
     knex('Pets').insert({
+      id: 1,
       Type: 'Finchet'
-    }),
-
-    // Resources Table
-    knex('Resources').del(),
+    })
+  ]);
+}).then(function(){
+  return Promise.all([
+    knex('Resource_Rarity').insert({Rarity: 'Common'}),
+    knex('Resource_Rarity').insert({Rarity: 'Rare'}),
+    knex('Resource_Rarity').insert({Rarity: 'Epic'})
+  ]);
+}).then(function(){
+  return Promise.all([
     knex('Resources').insert({
+      id: 1,
       Name: 'Corn',
       Value: '2',
       Rarity: 'Common'
     }),
     knex('Resources').insert({
+      id: 2,
       Name: 'Meat',
       Value: '5',
       Rarity: 'Rare'
-    }),
-
-    // Adoptions Table
-    knex('Adoptions').del(),
+    })
+  ]);
+}).then(function(){
+  return Promise.all([
     knex('Adoptions').insert({
       User_Id: 1,
       Pet_Id: 1,
@@ -90,9 +122,9 @@ exports.seed = function(knex, Promise) {
       Current_Energy: 5,
       Last_Updated: '2016/01/01'
     }),
-
-    // User_Resources Table
-    knex('User_Resources').del(),
+  ]);
+}).then(function(){
+  return Promise.all([
     knex('User_Resources').insert({
       User_Id: 1,
       Resource_Id: 1,
@@ -113,5 +145,6 @@ exports.seed = function(knex, Promise) {
       Resource_Id: 1,
       Quantity: 5
     })
-  )
+  ]);
+});
 };
