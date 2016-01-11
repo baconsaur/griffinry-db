7f085a881442fc4500d2b09bc39613e94fc1feeb
J. Drill <JJDrill@hotmail.com>
2016-01-11 11:09:27 -0700

    Updated all table names to be lower case

--- migrations/20160107214930_Resource_Rarity.js
+++ migrations/20160107214930_Resource_Rarity.js
@@ -1,10 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('Resource_Rarity', function(table){
+  return knex.schema.createTable('resource_rarity', function(table){
     table.string('Rarity').notNullable().primary();
   })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('Resource_Rarity');
+  return knex.schema.dropTable('resource_rarity');
 };
--- migrations/20160107222700_User_Resources.js
+++ migrations/20160107222700_User_Resources.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('User_Resources', function(table){
-    table.integer('User_Id').notNullable().references('id').inTable('Users');
-    table.integer('Resource_Id').notNullable().references('id').inTable('Resources');
+  return knex.schema.createTable('user_resources', function(table){
+    table.integer('User_Id').notNullable().references('id').inTable('users');
+    table.integer('Resource_Id').notNullable().references('id').inTable('resources');
     table.integer('Quantity').notNullable();
     table.primary(['User_Id', 'Resource_Id']);
   })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('User_Resources');
+  return knex.schema.dropTable('user_resources');
 };
